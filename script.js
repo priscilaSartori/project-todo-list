239789d72d8b23fe5aa2c1d8d5ff3bf130d9ad5d
Priscila <priscilasartori2@gmail.com>
2022-12-11 12:41:36 -0300

    Requisitos 5-6

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ const buttonSalvarTarefas = document.querySelector('#salvar-tarefas');
 const moverParaCima = document.getElementById('mover-cima');
 const moverParaBaixo = document.getElementById('mover-baixo');
 const buttonRemoverSelecionado = document.getElementById('remover-selecionado');
+
+const novaTarefa = () => {
+  const newLi = document.createElement('li');
+  newLi.innerText = input.value;
+  newLi.id = 'semBackground';
+  newLi.className = 'semCompleted';
+  ol.appendChild(newLi);
+
+  input.value = '';
+};
+
+button.addEventListener('click', novaTarefa);
